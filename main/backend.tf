f33b30494ee3f3056030a5cedc1a95466d7eb3de
RaviTeja-Reddy <112524666+RaviTeja-Reddy@users.noreply.github.com>
2022-09-29 01:21:37 +0100

    Delete backend.tf

--- main/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "ravi_rg_back"
-    storage_account_name = "ravibackend"
-    container_name       = "backendconti"
-    key                  = "prod.terraform.tfstate"
-  }
-}
